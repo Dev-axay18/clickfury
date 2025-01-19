4e125e206259d132a01407c0b79d2f5540b22899
Akshay Kale <162922094+Dev-axay18@users.noreply.github.com>
2025-01-19 20:40:00 +0530

    Uploaded app.jsx files (Essentials)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+
+import TypingTest from "./components/TypingTest";
+
+function App() { 
+  return (
+    <div className="flex align-center justify-center overflow-hidden">
+      <TypingTest />
+    </div>
+  );
+}
+
+export default App;
